f79c220ab1906191ae2d06086d4d19c58fcd0346
Liz Miller <liz-miller@users.noreply.github.com>
2014-11-10 18:06:41 -0500

    Update lmiller_lab2.py
    revision 1.1

--- lmiller_lab2.py
+++ lmiller_lab2.py
@@ -2,6 +2,7 @@
 
 # ROS python declaration
 import rospy, tf
+import math
 
 # Imports for each of the message types used
 from kobuki_msgs.msg import BumperEvent
@@ -9,19 +10,18 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
 # These are global variables. Write "global <variable_name>" in any other function
-#  to gain access to these global variables
+# to gain access to these global variables
     
 global pub
 global pose
 global odom_tf
 global odom_list
-global radians = 0.01745
 
-sub = rospy.Subscriber("/cmd_vel",Twist,read_odometry, queue_size=1) # Callback function to read in robot Odometry messages
+sub = rospy.Subscriber("/cmd_vel_mux/input/teleop",Twist,read_odometry, queue_size=1) # Callback function to read in robot Odometry messages
     
 bumper_sub = rospy.Subscriber('...', ..., readBumper, queue_size=1) # Callback function to handle bumper events
     
-# Use this object to get the robot's Odometry
+# Use this object to get the robot's Odometryn
     odom_list = tf.TransformListener()
     
 # Use this command to make the program wait for some seconds
@@ -29,8 +29,21 @@ bumper_sub = rospy.Subscriber('...', ..., readBumper, queue_size=1) # Callback f
 
 #This function sequentially calls methods to perform a trajectory.
 def executeTrajectory():
-    pass  # Delete this 'pass' once implemented
+    print "drive fwd .60m"
+    driveStraight(0.1,0.60)
+    driveStraight(0,0) # stop wheels
+    
+    print "r-turn 90-deg"
+    rotate(90)
+    
+    print "drive -180 arc, .15m radius"
+    driveArc(-180,0.1,0.15)
 
+    print "l-turn 135-deg"
+    rotate(135)
+    
+    print "drive fwd .42m"
+    driveStraight(0.1,0.42)
 
 #This function accepts two wheel velocities and a time interval.
 def spinWheels(u1, u2, time):
@@ -49,6 +62,8 @@ def rotate(angle):
 def driveArc(radius, speed, angle):
     pass  # Delete this 'pass' once implemented
 
+################## CALLBACK FUNCTIONS ##################
+
 #Odometry Callback function.
 def read_odometry(msg):
     # sign that callback works
@@ -61,25 +76,11 @@ def read_odometry(msg):
 #Bumper Event Callback function
 def readBumper(msg):
     if (msg.state == 1):
-    
     # message that bumper has been pressed
     rospy.loginfo("bumper triggered")
     
-    print "drive fwd .60m"
-    driveStraight(0.1,0.60)
-    driveStraight(0,0) # stop wheels
-    
-    print "r-turn 90-deg"
-    rotate(90)
-    
-    print "drive -180 arc, .15m radius"
-    driveArc(-180,0.1,0.15)
-
-    print "l-turn 135-deg"
-    rotate(135)
-    
-    print "drive fwd .42m"
-    driveStraight(0.1,0.42)
+    # setup the subscriber
+    rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, readBumper)
         
 # (Optional) If you need something to happen repeatedly at a fixed interval, write the code here.
 # Start the timer with the following line of code: 
@@ -91,13 +92,11 @@ def timerCallback(event):
 # Main Function
 if __name__ == '__main__':
    
-    pub = rospy.Publisher('cmd_vel', Twist) # Publisher for commanding robot motion
+    pub = rospy.Publisher("/mobile_base/commands/velocity", Twist) # Publisher for commanding robot motion
     rospy.init_node('lmiller-lab2')
     
-   
-
     print "Starting Lab 2"
-    
+        
     # Subscribe to turtlebot
 
     print "Lab 2 complete!"
